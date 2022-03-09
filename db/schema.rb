037a8dbb001b1c2cd55784b41935d46faa8a4f6a
Allan Costa <allan.da.silva.costa@gmail.com>
2022-03-09 18:27:10 -0300

    Installed Gem Devise - Generated Devise Models, Controllers, Views, Authenticators and Helpers, and added Authentication condtion to all future pages with the exception of The Homepage

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_03_09_212342) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "presents", force: :cascade do |t|
+    t.string "category"
+    t.float "price"
+    t.string "image_url"
+    t.string "item_url"
+    t.text "description"
+    t.string "name"
+    t.integer "rating"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
 
 end
