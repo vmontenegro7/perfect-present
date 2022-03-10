af48d4c26d56754e4c300ef02d981f71b717ee55
Valeria Montenegro <valemontenegro7@gmail.com>
2022-03-10 08:36:33 -0500

    View pages and schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2022_03_09_212342) do
-
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,7 +25,7 @@ ActiveRecord::Schema.define(version: 2022_03_09_212342) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
-  
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -40,3 +37,4 @@ ActiveRecord::Schema.define(version: 2022_03_09_212342) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+end
