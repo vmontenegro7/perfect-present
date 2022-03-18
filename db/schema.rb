07eaf57f4127a68845807648f5805184f65eb22b
Bernard van der Wal <bernardvanderwal@gmail.com>
2022-03-18 15:33:36 -0300

    Changes made to the footer, trying to make homepage responsive.

--- db/schema.rb
+++ db/schema.rb
@@ -7,6 +7,7 @@
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
+#
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2022_03_16_165350) do
